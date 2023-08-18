40f76e311befc1cb2f5b820e7e96629b2fee02e2
ES71506891Y <oscar.neira@enel.com>
2023-08-18 05:40:26 +0200

    Tries to generate desktop links when installing with pip. Configuration has been moved from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+import setuptools
+from setuptools.command.install import install
 
-setup(
-    name='ong_gesfincas',
-    version='0.1.1',
-    packages=['liquidaciones'],
-    url='www.neirapinuela.es',
-    license='',
-    author='Oscar Neira',
-    author_email='oneirag@yahoo.es',
-    description=('Macro para procesar ficheros de gesfincas (fusionando los datos de varias fincas que vienen en hojas '
-                 'separadas en dos hojas, una para ingresos y otra para gastos) y otra macro que crea una ventana con '
-                 'la que puntear los datos del banco contra los datos anteriores'),
-    entry_points={
-            'console_scripts': [
-                'liquidaciones=liquidaciones.liquidaciones_gui:main',
-                'punteo=liquidaciones.conciliation_gui:main',
-            ],
-        },
-    install_requires=required,
-)
+
+def post_install():
+    """Creates a shortcut in desktop after install"""
+    # TODO: find PYTHONHOME/Lib/site-packages/{package}{version}.dist-info, add an extra line with the shortcut
+    import os.path
+
+    from pyshortcuts import make_shortcut, platform, shortcut, get_folders
+
+    iconfile = 'shovel.icns' if platform.startswith('darwin') else 'shovel.ico'
+    script = '_ -m liquidaciones.conciliation_gui'
+    name = 'Punteo de cuentas'
+    scut = shortcut(script=script, name=name, userfolders=get_folders())
+    scut_filename = os.path.join(scut.desktop_dir, scut.target)
+    # Not needed: shortcuts are not overwritten using make_shortcut
+    # if os.path.exists(scut_filename):
+    #     os.remove(scut_filename)
+    retva = make_shortcut(script=script, name=name, icon=None,
+                          description="Lanzador de la interfaz del punteo de cuentas",
+                          startmenu=False)
+
+
+class PostInstall(install):
+    def run(self):
+        install.run(self)
+        post_install()
+
+
+if __name__ == "__main__":
+    setuptools.setup()
