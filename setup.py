427fe1298903f5790690fd1280aa9c440dcc815e
Oscar Neira Garcia <oneirag@yahoo.es>
2023-08-20 09:10:03 +0200

    allow uninstall of created shortcut with pip uninstall

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ import setuptools
 from setuptools.command.install import install
 
 
-def post_install():
+def post_install(self):
     """Creates a shortcut in desktop after install"""
-    # TODO: find PYTHONHOME/Lib/site-packages/{package}{version}.dist-info, add an extra line with the shortcut
-    import os.path
 
+    import os
     from pyshortcuts import make_shortcut, platform, shortcut, get_folders
 
     iconfile = 'shovel.icns' if platform.startswith('darwin') else 'shovel.ico'
@@ -23,11 +22,28 @@ def post_install():
                           description="Lanzador de la interfaz del punteo de cuentas",
                           startmenu=False)
 
+    lib_base = self.install_lib
+    package = self.distribution.metadata.name
+    version = self.distribution.metadata.version
+    python_version = self.config_vars.get('py_version_short')
+    for inst_type in "dist", "egg":
+        install_basepath = f"{lib_base}{package}-{version}-py{python_version}.{inst_type}-info"
+        if os.path.isdir(install_basepath):
+            if inst_type == "dist":
+                # Look for RECORD file and add shortcut info
+                record_file = os.path.join(install_basepath, "RECORD")
+                # TODO: process RECORD file
+            elif inst_type == "egg":
+                # add line to the (could be non-existing) file installed-files.txt
+                with open(os.path.join(install_basepath, "installed-files.txt"), "a") as f:
+                    f.writelines([scut_filename])
+
 
 class PostInstall(install):
     def run(self):
+        print(self)
         install.run(self)
-        post_install()
+        post_install(self)
 
 
 if __name__ == "__main__":
