dd4a1d9d0f6c89f9124b7caadfcd2987e13930b8
Oscar Neira Garcia <oneirag@yahoo.es>
2023-08-21 02:58:18 +0200

    Added process of wheel files

--- setup.py
+++ setup.py
@@ -29,25 +29,33 @@ class PostInstall(install):
                               description="Lanzador de la interfaz del punteo de cuentas",
                               startmenu=False)
 
+        with open(scut_filename, "r") as f:
+            scut_content = f.read()
+            len_scut = len(scut_content)
         lib_base = self.install_lib
         package = self.distribution.metadata.name
         version = self.distribution.metadata.version
         python_version = self.config_vars.get('py_version_short')
         for inst_type in "dist", "egg":
-            for python_version in self.config_vars.get('py_version_short'), None:
-                python_version_str = f"-py{python_version}" if python_version else ""
-                install_basepath = f"{lib_base}{package}-{version}{python_version_str}.{inst_type}-info"
-                if os.path.isdir(install_basepath):
-                    if inst_type == "dist":
-                        # Look for RECORD file and add shortcut info
-                        # TODO: verify processing of RECORD file
-                        record_file = os.path.join(install_basepath, "RECORD")
-                        with open(record_file, "a") as f:
-                            f.writelines([scut_filename + ",,"])
-                    elif inst_type == "egg":
-                        # add line to the (could be non-existing) file installed-files.txt
-                        with open(os.path.join(install_basepath, "installed-files.txt"), "a") as f:
-                            f.writelines([scut_filename])
+            install_path = "{lib_base}{package}-{version}{python_version_str}.{inst_type}-info".format(
+                lib_base=lib_base, package=package, version=version,
+                python_version_str=f"-py{python_version}" if "egg" else "",
+                inst_type=inst_type
+            )
+            if os.path.isdir(install_path):
+                if inst_type == "dist":
+                    # Look for RECORD file and add shortcut info
+                    # TODO: verify processing of RECORD file
+                    record_file = os.path.join(install_path, "RECORD")
+                    import hashlib
+                    import base64
+                    sha256 = base64.urlsafe_b64encode(hashlib.sha256(scut_content.encode()).digest())
+                    with open(record_file, "a") as f:
+                        f.writelines([f"{scut_filename},sha256={sha256},{len_scut}"])
+                elif inst_type == "egg":
+                    # add line to the (could be non-existing) file installed-files.txt
+                    with open(os.path.join(install_path, "installed-files.txt"), "a") as f:
+                        f.writelines([scut_filename])
 
 
 if __name__ == "__main__":
